6d1f31d755b76bb206ca96ac659aff85455500ca
maulanairfanf <maulanairfanf@gmail.com>
2023-12-19 12:42:06 +0700

    header

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,15 +1,29 @@
-import { StyleSheet, View, Text } from 'react-native'
+import { TouchableOpacity, Text, View } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import styles from './home.style'
+import { Ionicons, Fontisto } from "@expo/vector-icons"
 
 const Home = () => {
   return (
     <SafeAreaView>
-      <Text>Home</Text>
+      <View style={styles.appBarWrapper}>
+        <View style={styles.appBar}>
+          <Ionicons name="location-outline" size={24} />
+          <Text style={styles.location} >Shaingai Cina</Text>
+          <View style={{ alignItems: "flex-end" }} >
+            <View style={styles.cartCount} >
+              <Text style={styles.cartNumber} > 8 </Text>
+            </View>
+            <TouchableOpacity >
+              <Fontisto name="shopping-bag" size={24} />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
     </SafeAreaView>
   )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
