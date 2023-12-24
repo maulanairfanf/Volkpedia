136a10c7f4d4b83ba41fd520260f6ab62ec8bca3
maulanairfanf <maulanairfanf@gmail.com>
2023-12-24 22:28:37 +0700

    example store

--- components/home/Welcome.jsx
+++ components/home/Welcome.jsx
@@ -1,13 +1,32 @@
 import { View, Text, TouchableOpacity } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
 import styles from './welcome.style'
 import { COLORS, SIZES } from '../../constants'
 import { Feather, Ionicons } from "@expo/vector-icons"
 import { TextInput } from 'react-native-gesture-handler'
 import { useNavigation } from '@react-navigation/native'
+import * as SecureStore from 'expo-secure-store';
+
+async function save(key, value) {
+  await SecureStore.setItemAsync("token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1ODdiYWE0MGM5NThjYWUzMDY5MmQ0NyIsImlhdCI6MTcwMzM5NzM4NiwiZXhwIjoxNzA1OTg5Mzg2fQ.yBkiErwOF-yHD8zCfNE_rknWB_JOxZAv2M3agVR6AS4");
+}
+
+async function getValueFor(key) {
+  let result = await SecureStore.getItemAsync("token");
+  if (result) {
+    alert("🔐 Here's your value 🔐 \n" + result);
+  } else {
+    alert('No values stored under that key.');
+  }
+}
 
 const Welcome = () => {
   const navigation = useNavigation()
+
+  useEffect(() => {
+    save()
+    getValueFor()
+  },[])
   return (
     <View style={styles.container} > 
       <Text style={styles.welcomeText(COLORS.black, SIZES.xSmall)}>
--- hooks/axios.js
+++ hooks/axios.js
@@ -1,22 +1,24 @@
 import axios from "axios";
 import {BASE_URL} from '@env'
 
+
 const api = axios.create({
   baseURL: BASE_URL
 })
 
-// api.interceptors.request.use(
-//   config => {
-//     if (!config.headers.Authorization) {
-//       config.headers.Authorization = `Bearer ${authContext.getAccessToken()}`;
-//     }
+api.interceptors.request.use(
+  config => {
+    // if (!config.headers.Authorization) {
+    //   config.headers.Authorization = `Bearer ${authContext.getAccessToken()}`;
+    // }
+    config.headers.Authorization = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1ODdiYWE0MGM5NThjYWUzMDY5MmQ0NyIsImlhdCI6MTcwMzM5NzM4NiwiZXhwIjoxNzA1OTg5Mzg2fQ.yBkiErwOF-yHD8zCfNE_rknWB_JOxZAv2M3agVR6AS4`;
 
-//     return config;
-//   },
-//   error => {
-//     return Promise.reject(error);
-//   },
-// )
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  },
+)
 
 api.interceptors.response.use((response) => {
     return response
