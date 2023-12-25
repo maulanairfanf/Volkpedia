4b99411c4671ebbb7133cbf5eaa580e28a6b159d
maulanairfanf <maulanairfanf@gmail.com>
2023-12-25 22:15:11 +0700

    ui login

--- screens/SignIn.jsx
+++ screens/SignIn.jsx
@@ -1,12 +1,76 @@
-import { View, Text, SafeAreaView } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, Image, TextInput, Button, Pressable } from 'react-native'
+import React, { useState } from 'react'
 import styles from './signIn.style'
+import { Feather, Ionicons } from "@expo/vector-icons"
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
+import { COLORS } from '../constants';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 const SignIn = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [showPassword, setShowPassword] = useState(false)
+
+  const toggleShowPassword = () => { 
+    setShowPassword(!showPassword); 
+  };
+
   return (
-    <SafeAreaView>
-      <Text>SignIn</Text>
-    </SafeAreaView>
+    <KeyboardAwareScrollView>
+      <View style={styles.container}>
+        <Image 
+          style={styles.image} 
+          source={require("../assets/images/bk.png")}
+        />
+        <Text style={styles.title}>Unlimited Luxurious Furniture</Text>
+        <View style={styles.inputContainer}>
+          <View>
+            <Feather name="mail" size={24} style={styles.inputIcon} />
+          </View>
+          <View style={styles.inputWrapper}>
+            <TextInput
+              style={styles.input}
+              value={email}
+              onChangeText={setEmail}
+              placeholder="Enter email"
+            />
+          </View>
+        </View>
+        <View style={styles.inputContainer}>
+          <View>
+            <Feather name="lock" size={24} style={styles.inputIcon} />
+          </View>
+          <View style={styles.inputWrapper}>
+            <TextInput
+              secureTextEntry={!showPassword} 
+              style={styles.input}
+              value={password}
+              onChangeText={setPassword}
+              placeholder="Password"
+            />
+          </View>
+           <View>
+            <TouchableOpacity>
+              <Feather 
+                name={showPassword ? 'eye' : 'eye-off'} 
+                size={24} 
+                style={styles.inputIcon}
+                onPress={toggleShowPassword} 
+
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View style={styles.buttonContainer}>
+          <Pressable
+            style={styles.button}
+            onPress={() => {}}
+          > 
+            <Text style={styles.textButton}>LOGIN</Text>
+          </Pressable>
+        </View>
+      </View>
+    </KeyboardAwareScrollView>
   )
 }
 
--- screens/signIn.style.js
+++ screens/signIn.style.js
@@ -2,7 +2,65 @@ import { StyleSheet } from "react-native"
 import { COLORS, SIZES } from "../constants"
 
 const styles = StyleSheet.create({
-
+  container: {
+    marginHorizontal: SIZES.small,
+    alignItems: 'center'
+  },
+  image: {
+    width: "100%",
+    height:500,
+    resizeMode: "contain"
+  },
+  title: {
+    fontFamily: "bold",
+    color: COLORS.primary,
+    fontSize: SIZES.large,
+    marginTop: SIZES.small
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignContent: "center",
+    marginHorizontal: SIZES.small,
+    backgroundColor: COLORS.secondary,
+    borderRadius: SIZES.small,
+    marginTop: SIZES.large,
+    height: 50
+  },
+  inputIcon: {
+    marginHorizontal: 10,
+    color: COLORS.gray,
+    marginTop: SIZES.small
+  },
+  inputWrapper:{
+    flex: 1, 
+    backgroundColor: COLORS.secondary,
+    marginRight: SIZES.small,
+    borderRadius: SIZES.small
+  },
+  input: {
+    fontFamily: "regular",
+    width: "100%",
+    height: "100%",
+    paddingHorizontal: SIZES.small
+  },
+  buttonContainer: {
+    marginTop: SIZES.medium,
+    flex: 1,
+    width: SIZES.width,
+    paddingHorizontal: SIZES.xLarge,
+    marginTop: SIZES.large
+  },
+  button: {
+    backgroundColor: COLORS.primary,
+    alignItems: "center",
+    paddingVertical: SIZES.xSmall - 4,
+    borderRadius: SIZES.xSmall
+  },
+  textButton: {
+    color: COLORS.lightWhite,
+    fontFamily: "bold"
+  }
 })
 
 export default styles
