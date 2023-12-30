01d589e013a20e9cec0e1b791ff7f74f86684021
maulanairfanf <maulanairfanf@gmail.com>
2023-12-30 22:00:31 +0700

    ui

--- screens/SignIn.jsx
+++ screens/SignIn.jsx
@@ -1,11 +1,22 @@
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, Image } from 'react-native'
 import React from 'react'
 import styles from './signIn.style'
 
 const SignIn = () => {
   return (
     <SafeAreaView>
-      <Text>SignIn</Text>
+      <View style={styles.container}>
+        <View style={{alignItems: "center"}}>
+          <Image 
+            source={require('../assets/images/bk.png')}
+            style={styles.image}
+            />
+          <View style={styles.containerEmail}>
+            <Text style={styles.name}>Unlimited Luxurious Furniture</Text>
+            
+          </View>
+        </View>
+      </View>
     </SafeAreaView>
   )
 }
--- screens/signIn.style.js
+++ screens/signIn.style.js
@@ -2,7 +2,13 @@ import { StyleSheet } from "react-native"
 import { COLORS, SIZES } from "../constants"
 
 const styles = StyleSheet.create({
+  container: {
 
+  },
+  image: {
+    aspectRatio: 1,
+    resizeMode: "cover"
+  }
 })
 
 export default styles
